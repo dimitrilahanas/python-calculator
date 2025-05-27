b89d934e18bd2fb11d67ede6594cefc55892643a
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-05-28 00:01:52 +1000

    Feature (calculator.py)
    > operation_selector function
    > completed runner function

--- calculator.py
+++ calculator.py
@@ -2,11 +2,14 @@ class Calculator:
     def __init__(self):
         self.a = None
         self.b = None
+        self.operation = None
 
+    # User Input
     def user_input(self):
         self.a = int(input("First Number? "))
         self.b = int(input("Second Number? "))
 
+    # Supported Operators
     def operation_list(self):
         operations = ["+ Addition", 
                       "- Subtraction", 
@@ -15,6 +18,7 @@ class Calculator:
         for operation in operations:
             print(operation)
 
+    # Math Logic
     def addition(self):
         return self.a + self.b
     
@@ -26,7 +30,23 @@ class Calculator:
     
     def division(self):
         return self.a / self.b
+    
+    # User Operation Input
+    def operation_selector(self):
+        self.operation = str(input("Select an operator: "))
+        
+        match self.operation:
+            case "+":
+                print("Result: " + str(self.addition()))
+            case "-":
+                print("Result: " + str(self.subtraction()))
+            case "*":
+                print("Result: " + str(self.multiplication()))
+            case "/":
+                print("Result: " + str(self.division()))
 
+    # Runner
     def run(self):
         self.user_input()
         self.operation_list()
+        self.operation_selector()

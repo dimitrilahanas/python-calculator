abcfc5d902c37ba8c37aaffcab1bb28957f68385
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-05-27 23:51:52 +1000

    Feature (calculator.py)
    > operation_list function - Lists all supported functions
    
    > addition, subtraction, multiplication, division functions

--- calculator.py
+++ calculator.py
@@ -7,5 +7,26 @@ class Calculator:
         self.a = int(input("First Number? "))
         self.b = int(input("Second Number? "))
 
+    def operation_list(self):
+        operations = ["+ Addition", 
+                      "- Subtraction", 
+                      "* Multiplication", 
+                      "/ Division"]
+        for operation in operations:
+            print(operation)
+
+    def addition(self):
+        return self.a + self.b
+    
+    def subtraction(self):
+        return self.a - self.b
+    
+    def multiplication(self):
+        return self.a * self.b
+    
+    def division(self):
+        return self.a / self.b
+
     def run(self):
         self.user_input()
+        self.operation_list()
